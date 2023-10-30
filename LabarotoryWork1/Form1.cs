e3a741adb99b034d38c8325989e158c982174da3
OnePuchPotato <73746820+Jibek1906@users.noreply.github.com>
2023-10-30 22:05:15 +0600

    Lab1

---
+++ LabarotoryWork1/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace LabarotoryWork1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a, b, c;
+            string Operation = textBox3.Text;
+            a = Convert.ToInt32(textBox1.Text);
+            b = Convert.ToInt32(textBox2.Text);
+
+            switch (Operation)
+            {
+                case "+":
+                    c = a + b;
+                    break;
+                case "-":
+                    c = a - b;
+                    break;
+                case "*":
+                    c = a * b;
+                    break;
+                case "/":
+                    if (b != 0)
+                    {
+                        c = a / b;
+                    }
+                    else
+                    {
+                        textBox4.Text = "Error: Division by zero";
+                        return; 
+                    }
+                    break;
+                default:
+                   
+                    textBox4.Text = "Error: Invalid operation";
+                    return;
+            }
+
+            textBox4.Text = c.ToString();
+        }
+    }
+}
